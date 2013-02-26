9a0a4ef2e9d297bafe619484e347a10b6e6ef140
Francisco Sokol <chico.sokol@gmail.com>
2013-02-26 18:59:07 -0300

    first commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipOutputStream;
+
+import play.Configuration;
+import play.Play;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+import br.com.caelum.tubaina.ParseType;
+import br.com.caelum.tubaina.TubainaBuilder;
+
+public class Application extends Controller {
+  
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+    
+    public static Result generate(String course, String type) throws IOException {
+        Configuration configuration = Play.application().configuration();
+        String tempDir = configuration.getString("tubaina.output.dir");
+        String apostilasDir = configuration.getString("tubaina.apostilas.dir");
+        String templatePath = configuration.getString("tubaina.templates.dir");
+        File outputDir = new File(tempDir, System.currentTimeMillis() + "");
+        File templateDir = new File(templatePath);
+        
+        runTubaina(course, type, apostilasDir, outputDir, templateDir);
+        
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        zipOutput(outputDir, byteArrayOutputStream);
+        
+        response().setHeader("Content-Disposition", "attachment; filename=" + course + ".zip");
+        return ok(byteArrayOutputStream.toByteArray());
+    }
+
+    private static void zipOutput(File outputDir, ByteArrayOutputStream byteArrayOutputStream)
+            throws IOException {
+        Zipper zipper = new Zipper();
+        ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
+        zipper.addDirectory(zipOutputStream, outputDir);
+        
+        byteArrayOutputStream.close();
+        zipOutputStream.finish();
+    }
+
+    private static void runTubaina(String course, String type, String apostilasDir, File outputDir,
+            File templateDir) throws IOException {
+        TubainaBuilder tubainaBuilder = new TubainaBuilder(ParseType.LATEX);
+        tubainaBuilder.bookName(course);
+        tubainaBuilder.templateDir(templateDir);
+        tubainaBuilder.inputDir(new File(apostilasDir + course));
+        outputDir.mkdirs();
+        tubainaBuilder.outputDir(outputDir);
+        
+        if (type.equals("instrutor")) { 
+            tubainaBuilder.showNotes();
+        }
+        
+        tubainaBuilder.build();
+    }
+  
+}
---
+++ project/Build.scala
@@ -0,0 +1,37 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "tubaina-aas"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean,
+    
+    "net.htmlparser.jericho" % "jericho-html" % "3.1",
+	"de.java2html" % "java2html" % "5.0",
+	//"log4j" % "log4j" % "1.2.12",
+	"commons-io" % "commons-io" % "2.4",
+	"commons-cli" % "commons-cli" % "1.1",
+	"org.apache.httpcomponents" % "httpclient" % "4.1.3",
+	"org.freemarker" % "freemarker" % "2.3.19",
+	"com.google.code.gson" % "gson" % "2.1",
+	"com.lowagie" % "itext" % "2.1.7",
+	"com.thoughtworks.xstream" % "xstream" % "1.4.2",
+	"org.apache.commons" % "commons-exec" % "1.1",
+	"net.lingala.zip4j" % "zip4j" % "1.3.1"
+            
+    
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here
+    resolvers += "maven" at "https://repository.jboss.org/nexus/content/groups/public/"      
+  )
+
+}
