2e6e4202dbffee78bdb20ee8e69ae5eb23bd8688
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2013-08-09 18:51:02 -0300

    Case insensitive search

--- public/javascripts/filterList.js
+++ public/javascripts/filterList.js
@@ -3,14 +3,14 @@
 		filterList: function(list, titleSelector) {
 			return this.on('keyup', function() {
 				var $input = $(this);
-				var filterVal = $input.val();
+				var filterVal = $input.val().toLowerCase();
 				var $listItems = $(list).find('li')
 				$listItems.removeClass('filtered');
 				if (filterVal != "") {
 					$listItems.each(function() {
 						var $this = $(this);
 						var title = $this.find(titleSelector);
-						if (!title.text().contains(filterVal)) {
+						if (!title.text().toLowerCase().contains(filterVal)) {
 							$this.addClass('filtered');
 						}
 					});
